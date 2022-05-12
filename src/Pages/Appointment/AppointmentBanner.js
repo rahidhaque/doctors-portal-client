70cca13af3a5b36b8f96e8dd6cb7c85a3a6817cc
rahidhaque <ontirahid@yahoo.com>
2022-05-12 23:18:45 +0600

    Firebase Added

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -6,9 +6,9 @@ import 'react-day-picker/dist/style.css';
 const AppointmentBanner = ({ date, setDate }) => {
 
     return (
-        <div class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="min-w-xs sm:max-w-sm lg:max-w-lg md:max-w-md rounded-lg shadow-2xl" alt='Dentist Chair' />
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="min-w-xs sm:max-w-sm lg:max-w-lg md:max-w-md rounded-lg shadow-2xl" alt='Dentist Chair' />
                 <div>
                     <DayPicker
                         mode="single"
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -10,24 +10,24 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
     }
     return (
         <div className='relative'>
-            <input type="checkbox" id="booking-modal" class="modal-toggle" />
-            <div class="modal modal-bottom sm:modal-middle">
-                <div class="modal-box absolute top-28">
-                    <label for="booking-modal" class="btn btn-secondary btn-circle absolute right-2 top-2">✕</label>
-                    <h3 class="font-bold text-lg">Booking for: {name}</h3>
+            <input type="checkbox" id="booking-modal" className="modal-toggle" />
+            <div className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box absolute top-28">
+                    <label for="booking-modal" className="btn btn-secondary btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="font-bold text-lg">Booking for: {name}</h3>
                     <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-3 justify-items-center'>
-                        <input type="text" disabled readOnly value={format(date, 'PP')} class="input input-bordered w-full max-w-xs" required />
-                        <select name='slot' class="select select-bordered w-full max-w-xs" required>
+                        <input type="text" disabled readOnly value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" required />
+                        <select name='slot' className="select select-bordered w-full max-w-xs" required>
 
                             {
                                 slots.map(slot => <option key={treatment._id} value={slot}>{slot}</option>)
                             }
 
                         </select>
-                        <input type="text" name='name' placeholder="Your name" class="input input-bordered w-full max-w-xs" required />
-                        <input type="email" name='email' placeholder="Your email" class="input input-bordered w-full max-w-xs" required />
-                        <input type="number" name='phone' placeholder="Phone Number" class="input input-bordered w-full max-w-xs" required />
-                        <input type="submit" value="Submit" class="btn btn-secondary w-full max-w-xs" required />
+                        <input type="text" name='name' placeholder="Your name" className="input input-bordered w-full max-w-xs" required />
+                        <input type="email" name='email' placeholder="Your email" className="input input-bordered w-full max-w-xs" required />
+                        <input type="number" name='phone' placeholder="Phone Number" className="input input-bordered w-full max-w-xs" required />
+                        <input type="submit" value="Submit" className="btn btn-secondary w-full max-w-xs" required />
                     </form>
                 </div>
             </div>
